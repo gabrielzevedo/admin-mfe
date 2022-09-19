f85eff6c49c70f58b19df11bc64b2a875bcce1e6
Gabriel Azevedo <gabrielx86@live.com>
2022-09-19 14:50:49 -0300

    ugly workaround to solve type

--- apps/shell/pages/index.tsx
+++ apps/shell/pages/index.tsx
@@ -1,6 +1,8 @@
 import dynamic from 'next/dynamic';
 
-const DetailsButton = dynamic(
+const DetailsButton = dynamic<{ children: string }>(
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore: Cannot find module
   async () => import('orders/details-button'),
   {
     ssr: false,
@@ -13,7 +15,7 @@ export function Index() {
       teste
       <DetailsButton>Detalhes</DetailsButton>
     </>
-  )
+  );
 }
 
 export default Index;
